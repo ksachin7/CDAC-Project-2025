3ae0a10a73fc81eeebad2c8ac56d81e437f22b25
ksachin7 <isachin.imp@gmail.com>
2025-06-28 09:37:05 +0530

    minor changes

--- backend/src/main/java/com/cdac/acts/e_Valuation/EValuationApplication.java
+++ backend/src/main/java/com/cdac/acts/e_Valuation/EValuationApplication.java
@@ -2,6 +2,7 @@ package com.cdac.acts.e_Valuation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class EValuationApplication {
@@ -9,5 +10,4 @@ public class EValuationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EValuationApplication.class, args);
 	}
-
 }
