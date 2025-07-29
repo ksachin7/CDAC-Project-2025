f7135bf7929ecb0994b6b96e1192120eebf2c9aa
Pranav Cherkar <pranavcherkar.2002@gmail.com>
2025-07-29 15:42:26 +0530

    WIP: local changes before pulling from main

--- backend/src/main/java/com/cdac/acts/e_Valuation/service/MeetingService.java
+++ backend/src/main/java/com/cdac/acts/e_Valuation/service/MeetingService.java
@@ -1,20 +1,29 @@
 package com.cdac.acts.e_Valuation.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cdac.acts.e_Valuation.entity.Meeting;
 import com.cdac.acts.e_Valuation.repository.MeetingRepository;
 
+@Service
 public class MeetingService {
 	
 	@Autowired
 	private MeetingRepository meetingRepo;
 	
-	public void create(Long cid, Long iid) {
+	@Autowired
+	private EmailService emailService;
+	
+	public void create(Long cid, Long iid, String porpose) {
 		Meeting meet = new Meeting();
 		meet.setCandidateid(cid);
 		meet.setInterviewerid(iid);
+		meet.setPurpose(porpose);
 		meetingRepo.save(meet);
+		emailService.sendSimpleEmail("19dcs009@lnmiit.ac.in", "Testing", "hello");
 	}
+	
+	
 
 }
