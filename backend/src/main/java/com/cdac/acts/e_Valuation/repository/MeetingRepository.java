7845757b30cf0b5e2af76ac5b3836fd9331d794e
Bhumur Agrawal <bhumuragrawal@gmail.com>
2025-07-29 09:20:36 +0530

    Meeting Service

--- backend/src/main/java/com/cdac/acts/e_Valuation/repository/MeetingRepository.java
+++ backend/src/main/java/com/cdac/acts/e_Valuation/repository/MeetingRepository.java
@@ -1,9 +1,11 @@
 package com.cdac.acts.e_Valuation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cdac.acts.e_Valuation.entity.Meeting;
 
+@Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
 }
--- backend/src/main/java/com/cdac/acts/e_Valuation/service/MeetingService.java
+++ backend/src/main/java/com/cdac/acts/e_Valuation/service/MeetingService.java
@@ -1,20 +1,25 @@
 package com.cdac.acts.e_Valuation.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cdac.acts.e_Valuation.entity.Meeting;
 import com.cdac.acts.e_Valuation.repository.MeetingRepository;
 
+@Service
 public class MeetingService {
 	
 	@Autowired
 	private MeetingRepository meetingRepo;
 	
-	public void create(Long cid, Long iid) {
+	public void create(Long cid, Long iid, String porpose) {
 		Meeting meet = new Meeting();
 		meet.setCandidateid(cid);
 		meet.setInterviewerid(iid);
+		meet.setPurpose(porpose);
 		meetingRepo.save(meet);
 	}
+	
+	
 
 }
