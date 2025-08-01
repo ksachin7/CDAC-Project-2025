519c2dbf3539d8491031a4b43736d2efe21cabc2
Devdatt gautam <devg26092001@gmail.com>
2025-08-01 16:50:19 +0530

    added full websocket for dashboard meeting message

--- backend/src/main/java/com/cdac/acts/e_Valuation/repository/MeetingRepository.java
+++ backend/src/main/java/com/cdac/acts/e_Valuation/repository/MeetingRepository.java
@@ -1,11 +1,16 @@
 package com.cdac.acts.e_Valuation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.cdac.acts.e_Valuation.dto.MeetingCreate;
 import com.cdac.acts.e_Valuation.entity.Meeting;
 
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+
+	List<Meeting> findByCandidateidOrInterviewerid(Long userId, Long userId2);
 
 }
--- backend/src/main/java/com/cdac/acts/e_Valuation/service/MeetingService.java
+++ backend/src/main/java/com/cdac/acts/e_Valuation/service/MeetingService.java
@@ -1,8 +1,12 @@
 package com.cdac.acts.e_Valuation.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cdac.acts.e_Valuation.dto.MeetingCreate;
 import com.cdac.acts.e_Valuation.entity.Meeting;
 import com.cdac.acts.e_Valuation.repository.MeetingRepository;
 
@@ -15,13 +19,32 @@ public class MeetingService {
 	@Autowired
 	private EmailService emailService;
 	
-	public void create(Long cid, Long iid, String porpose) {
+	public Long create(Long cid, Long iid, String porpose) {
 		Meeting meet = new Meeting();
 		meet.setCandidateid(cid);
 		meet.setInterviewerid(iid);
 		meet.setPurpose(porpose);
 		meetingRepo.save(meet);
-		emailService.sendSimpleEmail("19dcs009@lnmiit.ac.in", "Testing", "hello");
+		//emailService.sendSimpleEmail("19dcs009@lnmiit.ac.in", "Testing", "hello");
+		return meet.getMeetingid();
+	}
+
+	public List<MeetingCreate> getMeetingsByUserId(Long userId) {
+	    List<Meeting> resList= meetingRepo.findByCandidateidOrInterviewerid(userId, userId);
+	    List<MeetingCreate> dtoList = new ArrayList<MeetingCreate>();
+	    
+	    for(Meeting item :resList) {
+	    	MeetingCreate dto = new MeetingCreate();
+	    	dto.setCandidateid(item.getCandidateid());
+	    	dto.setInterviewerid(item.getInterviewerid());
+	    	dto.setMeetingid(item.getMeetingid());
+	    	dto.setPurpose(item.getPurpose());
+	    	
+	    	dtoList.add(dto);
+	    	
+	    }
+	    
+	    return dtoList;
 	}
 	
 	
