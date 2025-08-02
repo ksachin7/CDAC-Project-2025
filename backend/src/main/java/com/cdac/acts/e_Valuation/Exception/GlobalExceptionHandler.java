530fe8419d3fb9f41ffb958876297ba12b0229ee
ksachin7 <isachin.imp@gmail.com>
2025-08-02 22:39:19 +0530

    Improve authentication exception handling logic

--- backend/src/main/java/com/cdac/acts/e_Valuation/Exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/cdac/acts/e_Valuation/Exception/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.cdac.acts.e_Valuation.Exception;
 
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import com.cdac.acts.e_Valuation.dto.ErrorResponse;
@@ -17,6 +22,15 @@ public class GlobalExceptionHandler {
 				HttpStatus.INTERNAL_SERVER_ERROR.value()
 		);
 		return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-		
 	}
+	
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
+        String errorMsg = ex.getBindingResult()
+                            .getAllErrors()
+                            .stream()
+                            .map(ObjectError::getDefaultMessage)
+                            .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body("Validation failed: " + errorMsg);
+    }
 }
