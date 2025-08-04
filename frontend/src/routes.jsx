f26d8a7c4b0d4517023c4d7c41ab9c6241874a07
Devdatt gautam <devg26092001@gmail.com>
2025-08-05 01:41:57 +0530

    router setup checkup

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
