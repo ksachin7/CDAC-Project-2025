a1662c7853424231563e91ee0bb3783e93a9714f
unknown <bhumuragrawal@gmail.com>
2025-08-04 20:28:53 +0530

    HistoryMeetingList, Rating and Review Added

---
+++ backend/src/main/java/com/cdac/acts/e_Valuation/dto/HistoryMeetingResponse.java
@@ -0,0 +1,16 @@
+package com.cdac.acts.e_Valuation.dto;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class HistoryMeetingResponse {
+	private Long historymeetingid;
+	private Long meetingid;
+	private LocalDate date;
+	private boolean happen;
+	private String purpose;
+	private int rating;
+	private String review;
+}
